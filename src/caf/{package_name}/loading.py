168c7041cd9a17815468e3ad8c5d0be3192cf43e
Isaac Scott <isaac.scott@transportforthenorth.com>
2023-11-21 11:32:23 +0000

    Zone connectors and n_link included

--- src/caf/{package_name}/loading.py
+++ src/caf/{package_name}/loading.py
@@ -41,25 +41,26 @@ def split_string_columns(df, column_name, n, cols: list):
     return df
 
 def internal_multi(df_int, i):
-    df_int.drop(columns=['n_node'], inplace=True)
-    df_int.set_index(['o', 'd', 'uc', 'route', 'abs_demand', 'pct_demand'], inplace=True)
+    df_int.drop('n_node', axis=1, inplace=True)
     df_int['Nodes'] = df_int['Nodes'].str.split(',')
     df_int = df_int.join(pd.DataFrame(df_int['Nodes'].tolist(), index=df_int.index))
-    drop_cols = ['Nodes', 0, i - 1, i]
+    drop_cols = ['Nodes', i]
     df_int.drop(columns=drop_cols, inplace=True)
     if len(df_int.columns) < 2:
         return
-    a = df_int[range(1, i - 2)].stack().to_frame()
-    b = df_int[range(2, i - 1)]
-    b.columns -= 1
-    b = b.stack().to_frame()
+    a = df_int[range(0, i - 1)]
+    b = df_int[range(1, i)].stack().to_frame()
+    a.columns += 1
+    a = a.stack().to_frame()
     a.columns = ['a']
     b.columns = ['b']
     links = a.join(b).reset_index()
-    links.rename(columns={'level_6': 'order'}, inplace=True)
-    links.set_index(['uc', 'o', 'd', 'route', 'order', 'a', 'b'], inplace=True)
+    links.rename(columns={'level_5': 'order'}, inplace=True)
+    links['n_link'] = i-1
+    links.set_index(['n_link', 'o', 'd', 'route', 'order', 'a', 'b'], inplace=True)
     return links
-def read_satpig(path_to_satpig):
+
+def read_satpig(path_to_satpig, include_connectors: bool = True):
     """
     Return a 'complete' satpig dataframe in long format.
     This can be translated/manipulated into different formats.
@@ -67,20 +68,18 @@ def read_satpig(path_to_satpig):
     cols = ['o', 'd', 'uc', 'route', 'abs_demand', 'pct_demand', 'n_node']
     df = pd.read_csv(path_to_satpig, sep=';', names=['dummy'])
     df = split_string_columns(df, 'dummy', 7, cols)
-    final = []
+    uc = df['uc'].unique()[0]
+    df.drop('uc', axis=1, inplace=True)
+    df.set_index(['o', 'd', 'route', 'abs_demand', 'pct_demand'], inplace=True)
     dfs = []
     for i in df['n_node'].unique():
-        dfs.append((df[df['n_node'] == i].deepcopy(), i))
-
+        dfs.append((df[df['n_node'] == i].copy(), i))
+    del(df)
     df_out = multiprocess(internal_multi, arg_list=dfs)
-    print('debugging')
-
-    return pd.concat(final)
+    return pd.concat(df_out), uc
 
 if __name__=="__main__":
-    df = read_satpig(r"Y:\Carbon\QCR_Assignments\07.Noham_to_NoCarb\2018\RotherhamBase_i8c_2018_TS2_v107_SatPig_uc4.csv")
-
-    # df = pd.read_parquet(r"E:\misc_scripts\format_tests\satpig.parquet")
-    df = pd.read_hdf(r"E:\misc_scripts\format_tests\satpig.h5", key='test', mode='r')
+    df, uc = read_satpig(r"Y:\Carbon\QCR_Assignments\07.Noham_to_NoCarb\2018\RotherhamBase_i8c_2018_TS2_v107_SatPig_uc4.csv")
+    # df = pd.read_hdf(r"E:\misc_scripts\format_tests\satpig_comp.h5", key='test', mode='r')
     df.to_hdf(r"E:\misc_scripts\format_tests\satpig_comp.h5", key='test', mode='w', complevel=1)
     print('debugging')
